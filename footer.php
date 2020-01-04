2da21619ab0a56d0bba2dfa0dd0f2acc44b14abd
matt-watson-design <53411083+matt-watson-design@users.noreply.github.com>
2020-01-04 20:42:46 +0000

    MW - Update

---
+++ footer.php
@@ -0,0 +1,37 @@
+					
+						
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<!-- Footer -->
+
+    <footer id="footer">
+				
+<p class="copyright">&copy; Matt Watson Design | 2020</p>
+
+    </footer>
+
+
+			</div>
+            
+            
+
+<!-- Scripts -->
+
+<script src="../js/jquery.min.js"></script>
+<script src="../js/lity.min.js"></script>
+
+
+</body>
+
+</html>
---
+++ portfolio/fogarty.php
@@ -0,0 +1,84 @@
+<?php include("../header.php"); ?>
+
+
+						
+<!-- Main Section -->
+
+<section class="portfolio-container main">
+
+  
+  <div class="portfolio-txt-dscrp-artwrk-container">
+  
+<div class="portfolio-container-txt-dscrp">
+  
+
+  
+  
+  <header class="major">
+    <h2>Fogarty</h2>
+  </header>
+  
+
+
+  
+  <p>Fogarty is a sub-brand of Dunelm that specialises in quality, affordable bedding.</p>
+
+<p>This project required a brand new journey to be designed in order to showcase some of the new product photography and unique selling points across the Fogarty range.</p>
+
+<p>The 'Fogarty Sleep Zone' was split into five categories offering tabbed sub-categories within them. This was all built from scratch using HTML/CSS, and utilised jQuery to load in and out the required sub-categories via the interactive tabs.</p>
+
+  <a href="fogarty/fogarty-landing" data-lity="" class="button primary">
+    Live Demo
+   </a>
+  
+  
+  </div>
+  
+
+  
+  
+<div class="portfolio-container-artwrk">
+  
+  <img src="../images/portfolio-fogarty-showcase.jpg">
+  
+  </div>
+  
+    </div>
+
+  
+  
+  
+<div class="portfolio-container-skills-tools">
+  
+  
+  <h3>Skills/Tools Used</h3>
+  
+  <p>Photoshop <span>/</span> Ilustrator <span>/</span>  UI Design <span>/</span> UX Design <span>/</span> Responsive Web Design <span>/</span> Brand Guidelines <span>/</span> HTML5 <span>/</span> CSS3 <span>/</span> CMS <span>/</span> jQuery <span>/</span> User Testing <span>/</span> Device Testing <span>/</span> Photography Shoot Liaison <span>/</span> Team Management</p><p>
+
+  
+  </p></div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+</section>
+
+
+
+
+<?php include("../footer.php"); ?>
+
+				
+	
